e463467684f05a42ef560197bd6016c6efa6cac0
AndrmattR24 <andrewmatiasrengifo@gmail.com>
2024-06-14 18:45:44 -0500

    creando Proyect Movie Phrases

---
+++ src/main/java/com/andrmatt/moviephrases/MoviephrasesApplication.java
@@ -0,0 +1,13 @@
+package com.andrmatt.moviephrases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviephrasesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviephrasesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andrmatt/moviephrases/MoviephrasesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrmatt.moviephrases;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviephrasesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
