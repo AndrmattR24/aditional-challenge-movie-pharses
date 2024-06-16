de03a8fa27f5a4a6c6fb07ce9da28e7b364c37a7
AndrmattR24 <andrewmatiasrengifo@gmail.com>
2024-06-16 14:59:12 -0500

    actualizando model Phrase

--- src/main/java/com/andrmatt/moviephrases/model/Phrase.java
+++ src/main/java/com/andrmatt/moviephrases/model/Phrase.java
@@ -2,12 +2,13 @@ package com.andrmatt.moviephrases.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Phrase {
 
     @Id
